e992132c579b3a0103cde62eb335ed4dd8ca0b05
Jie <zhengjie.work@outlook.com>
2025-09-22 12:22:36 +0800

    fix test

--- test/NcpAdminBlazor.Web.Tests/Fixtures/WebAppFixture.cs
+++ test/NcpAdminBlazor.Web.Tests/Fixtures/WebAppFixture.cs
@@ -1,43 +1,87 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
-using NcpAdminBlazor.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Testcontainers.MySql;
+using Testcontainers.RabbitMq;
+using Testcontainers.Redis;
 
 namespace NcpAdminBlazor.Web.Tests.Fixtures;
 
+// ReSharper disable once ClassNeverInstantiated.Global
 public class WebAppFixture : AppFixture<Program>
 {
-    private readonly TestContainerFixture _containers = new();
+    private RedisContainer _redisContainer = null!;
+    private RabbitMqContainer _rabbitMqContainer = null!;
+    private MySqlContainer _mySqlContainer = null!;
+    private static int _index;
+    private static int _portIndex = 8080;
+    private readonly int _instanceIndex;
+    private int _listeningPort;
 
-    protected override void ConfigureServices(IServiceCollection services)
+    public HttpClient DefaultClient { get; private set; } = null!;
+
+    public WebAppFixture()
     {
-        
+        _instanceIndex = _index++;
     }
-    
-    protected override async ValueTask SetupAsync()
+
+    protected override async ValueTask PreSetupAsync()
     {
-        await _containers.CreateVisualHostAsync("/");
-        using var scope = Services.CreateScope();
-        var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-        await dbContext.Database.MigrateAsync();
+        _redisContainer = new RedisBuilder()
+            .WithCommand("--databases", "1024").Build();
+        _rabbitMqContainer = new RabbitMqBuilder()
+            .WithUsername("guest").WithPassword("guest").Build();
+        _mySqlContainer = new MySqlBuilder()
+            .WithUsername("root").WithPassword("123456")
+            .WithEnvironment("TZ", "Asia/Shanghai")
+            .WithDatabase("mysql").Build();
+        await Task.WhenAll(_redisContainer.StartAsync(),
+            _rabbitMqContainer.StartAsync(),
+            _mySqlContainer.StartAsync());
+        await CreateVisualHostAsync("v" + _instanceIndex);
     }
 
     protected override void ConfigureApp(IWebHostBuilder builder)
     {
-        builder.UseSetting("ConnectionStrings:Redis",
-            _containers.RedisContainer.GetConnectionString() + ",defaultDatabase=0");
-        builder.UseSetting("ConnectionStrings:MySql",
-            _containers.MySqlContainer.GetConnectionString());
-        builder.UseSetting("RabbitMQ:Port", _containers.RabbitMqContainer.GetMappedPublicPort(5672).ToString());
-        builder.UseSetting("RabbitMQ:Username", "guest");
-        builder.UseSetting("RabbitMQ:Password", "guest");
-        builder.UseSetting("RabbitMQ:VirtualHost", "/");
-        builder.UseSetting("RabbitMQ:HostName", _containers.RabbitMqContainer.Hostname);
-        builder.UseEnvironment("Test");
+        builder.ConfigureAppConfiguration(cfg =>
+        {
+            cfg.AddInMemoryCollection(new Dictionary<string, string?>
+            {
+                ["ConnectionStrings:Redis"] = _redisContainer.GetConnectionString() + ",defaultDatabase=0",
+                ["ConnectionStrings:MySql"] = _mySqlContainer.GetConnectionString(),
+                ["RabbitMQ:HostName"] = _rabbitMqContainer.Hostname,
+                ["RabbitMQ:Port"] = _rabbitMqContainer.GetMappedPublicPort(5672).ToString(),
+                ["RabbitMQ:Username"] = "guest",
+                ["RabbitMQ:Password"] = "guest",
+                ["RabbitMQ:VirtualHost"] = "/"
+            });
+        });
+
+        _listeningPort = _portIndex++;
+        var url = $"http://*:{_listeningPort}";
+        builder.UseSetting(WebHostDefaults.ServerUrlsKey, url);
+        builder.UseEnvironment("Development");
     }
 
+    protected override ValueTask SetupAsync()
+    {
+        DefaultClient = CreateClient(c => c.BaseAddress = new Uri($"http://localhost:{_listeningPort}"));
+        return base.SetupAsync();
+    }
+    
     protected override ValueTask TearDownAsync()
     {
-        _containers.Dispose();
-        return ValueTask.CompletedTask;
+        //await _container.DisposeAsync();
+        //NOTE: there's no need to dispose the container here as it will be automatically disposed by testcontainers pkg when the test run finishes.
+        //      this is especially true if this AppFixture is used by many test-classes with WAF caching enabled.
+        //      so, in general - containers don't need to be explicitly disposed, unless you disable WAF caching.
+        return base.TearDownAsync();
+    }
+
+    private async Task CreateVisualHostAsync(string visualHost)
+    {
+        await _rabbitMqContainer.ExecAsync(["rabbitmqctl", "add_vhost", visualHost]);
+        await _rabbitMqContainer.ExecAsync([
+            "rabbitmqctl", "set_permissions", "-p", visualHost, "guest", ".*", ".*", ".*"
+        ]);
     }
 }
--- test/NcpAdminBlazor.Web.Tests/ProgramTests.cs
+++ test/NcpAdminBlazor.Web.Tests/ProgramTests.cs
@@ -1,34 +1,15 @@
-// using System.Net.Http.Json;
-// using NcpAdminBlazor.Infrastructure;
-// using Microsoft.AspNetCore.Mvc.Testing;
-// using Microsoft.EntityFrameworkCore;
-//
-// namespace NcpAdminBlazor.Web.Tests
-// {
-//     [Collection("web")]
-//     public class ProgramTests : IClassFixture<MyWebApplicationFactory>
-//     {
-//         private readonly MyWebApplicationFactory _factory;
-//         private readonly HttpClient _client;
-//
-//         public ProgramTests(MyWebApplicationFactory factory)
-//         {
-//             using (var scope = factory.Services.CreateScope())
-//             {
-//                 var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-//                 db.Database.Migrate();
-//             }
-//             
-//             _factory = factory;
-//             _client = factory.CreateClient();
-//         }
-//
-//         [Fact]
-//         public async Task HealthCheckTest()
-//         {
-//             var client = _factory.CreateClient();
-//             var response = await client.GetAsync("/health");
-//             response.IsSuccessStatusCode.ShouldBeTrue();
-//         }
-//     }
-// }
+using NcpAdminBlazor.Web.Tests.Fixtures;
+
+namespace NcpAdminBlazor.Web.Tests
+{
+    [Collection("web")]
+    public class ProgramTests(WebAppFixture app) : TestBase<WebAppFixture>
+    {
+        [Fact]
+        public async Task HealthCheckTestAsync()
+        {
+            var response = await app.DefaultClient.GetAsync("/health", TestContext.Current.CancellationToken);
+            response.IsSuccessStatusCode.ShouldBeTrue();
+        }
+    }
+}
