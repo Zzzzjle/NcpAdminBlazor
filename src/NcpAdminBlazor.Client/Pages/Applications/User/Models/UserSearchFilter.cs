65ccdd65f5229580f1ca9bc1ea32afbb6f587f8d
Jie <zhengjie.work@outlook.com>
2025-10-12 18:18:17 +0800

    feat(search): implement reusable query form component with dynamic item handling

---
+++ src/NcpAdminBlazor.Client/Pages/Applications/User/Models/UserSearchFilter.cs
@@ -0,0 +1,12 @@
+namespace NcpAdminBlazor.Client.Pages.Applications.User.Models;
+
+public class UserSearchFilter
+{
+    public string? Username { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Phone { get; set; }
+
+    public string? RealName { get; set; }
+}
