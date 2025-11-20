4298b6e473dc377263bc7d7fa3b2f967983fce3d
Jie <zhengjie.work@outlook.com>
2025-11-20 21:40:19 +0800

    deps: update package versions and add MySQL password parameter

--- src/NcpAdminBlazor.AppHost/Program.cs
+++ src/NcpAdminBlazor.AppHost/Program.cs
@@ -1,10 +1,15 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 // Add Redis infrastructure
-var redis = builder.AddRedis("Redis");
+var redis = builder.AddRedis("redis");
 
+var mysqlPassword = builder.AddParameter("mysql-password", secret: true);
 // Add MySQL database infrastructure
-var mysql = builder.AddMySql("Database")
+var mysql = builder.AddMySql("database", password: mysqlPassword)
+    // Configure the container to store data in a volume so that it persists across instances.
+    .WithDataVolume()
+    // Keep the container running between app host sessions.
+    .WithLifetime(ContainerLifetime.Persistent)
     .WithPhpMyAdmin()
     .AddDatabase("MySql", "dev");
 
